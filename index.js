6780baacde045edd15030fc31a89e164289ccfc4
Adrian Schneider <siradrian@gmail.com>
2014-10-10 22:02:49 -0700

    wip

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-var blessed = require('blessed');
-var program = blessed.program();
-var screen  = blessed.screen();
-var Qissues = require('./lib/qissues');
-
-process.title = 'qissues';
-
-var qissues = new Qissues(screen, {
-    cwd: process.argv[2]
-});
-
-qissues.start(process.argv[3] || 'listIssues');
+module.exports = require('./lib/app');
