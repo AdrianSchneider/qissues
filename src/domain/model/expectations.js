3a5670d7a6f4298c0460ee5cb3ac7cc3344c2d9f
Adrian Schneider <siradrian@gmail.com>
2015-10-14 07:17:04 -0700

    docs

--- src/domain/model/expectations.js
+++ src/domain/model/expectations.js
@@ -5,6 +5,15 @@ var joi             = require('joi');
 var Promise         = require('bluebird');
 var ValidationError = require('../../errors/validation');
 
+/**
+ * Defines a tracker's expectations from the user at a given point in time
+ *
+ * @param {Object} schema
+ *
+ *   {
+ *      fieldName: { type: string, required: bool, default: mixed, choices: Promise }
+ *   }
+ */
 module.exports = function Expectations(schema) {
 
   /**
@@ -37,16 +46,33 @@ module.exports = function Expectations(schema) {
       });
   };
 
+  /**
+   * Throws a ValidationError when the incoming data does not match the schema
+   *
+   * @param {Object} data - user input
+   * @return {Object} data - user input
+   * @throws {ValidationError} when invalid
+   */
   this.ensureValid = function(data) {
     var result = joi.validate(data, objectSchemaToJoi(schema));
     if (result.error) throw new ValidationError(result.error.message);
     return data;
   };
 
+  /**
+   * Converts the schema into a joi schema
+   *
+   * @return {Joi.Schema}
+   */
   var objectSchemaToJoi = function() {
     return joi.object(_.mapObject(schema, fieldSchemaToJoi));
   };
 
+  /**
+   * Returns the schema field as a joi schema
+   *
+   * @return {Joi.Schema}
+   */
   var fieldSchemaToJoi = function(field) {
     var node = joi[field.type]();
 
