ca9543d04640dda9a48dd7bc04628eec522e5775
Adrian Schneider <siradrian@gmail.com>
2015-11-17 23:26:14 -0800

    fix create

--- src/ui/controllers/createIssue.js
+++ src/ui/controllers/createIssue.js
@@ -16,15 +16,15 @@ module.exports = function(ui, tracker, logger) {
    */
   var main = function(filters) {
     ui.showLoading();
-    ui.getExpected(normalizer.getNewIssueRequirements(), filters)
+    ui.capture(normalizer.getNewIssueRequirements(), filters)
       .then(persistIssue)
-      .then(ui.viewIssue)
+      .then(ui.controller.viewIssue)
       .catch(Cancellation, function() {
-        ui.message('Cancelled').then(ui.listIssues);
+        ui.message('Cancelled').then(ui.controller.listIssues);
       })
       .catch(Error, function(error) {
         logger.error('Caught error: ' + error.stack);
-        ui.message(error.message, 5000).then(ui.listIssues);
+        ui.message(error.message, 5000).then(ui.controller.listIssues);
       });
   };
 
