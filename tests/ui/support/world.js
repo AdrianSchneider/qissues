206fe91538844ccb0efc639bad854035bf4a58fe
Adrian Schneider <siradrian@gmail.com>
2015-12-01 21:23:02 -0800

    clean up old pty stuff

--- tests/ui/support/world.js
+++ tests/ui/support/world.js
@@ -1,11 +1,36 @@
 'use strict';
 
-var pty = require('pty.js');
+var pty     = require('pty.js');
 var Promise = require('bluebird');
 var spawn   = require('child_process').spawn;
 
-function Tui() {
+function World() {
+  this.spawn = function(program, args) {
+    return new Promise(function(resolve, reject) {
+      var result = { 
+        code: null,
+        stdout: '',
+        stderr: ''
+      };
+
+      var ps = spawn(program, args || []);
+
+      ps.stdout.on('data', function(data) {
+        result.stdout += data.toString('utf-8');
+      });
+      ps.stderr.on('data', function(data) {
+        result.stderr += data.toString('utf-8');
+      });
+      ps.on('exit', function(code) {
+        result.code = code;
+        resolve(result);
+      });
+    });
+  };
+
   this.spawnInteractive = function(program, args, callback) {
+    throw new Error('wip');
+
     if(!args) args = [];
     if(this.program) this.program.destroy();
     this.program = pty.spawn('bash', [], {
@@ -29,5 +54,5 @@ function Tui() {
 }
 
 module.exports = function() {
-  //this.World = Tui;
+  this.World = World;
 };
