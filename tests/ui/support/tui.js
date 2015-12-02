c379c310de02850b268019df7f63ccc0d6dfa67b
Adrian Schneider <siradrian@gmail.com>
2015-12-01 18:39:33 -0800

    debugging

--- tests/ui/support/tui.js
+++ tests/ui/support/tui.js
@@ -4,8 +4,6 @@ var pty = require('pty.js');
 
 function Tui() {
   this.spawn = function(program, args, callback) {
-    // XXX temporary workaround for old version of node
-
     if(this.program) this.program.destroy();
     this.program = pty.spawn(program, args || [], {
       name: 'xterm-color',
@@ -14,12 +12,21 @@ function Tui() {
       env: process.env
     });
 
+    console.log('program', this.program);
+
     this.program.output = '';
     this.program.on('data', function(data) {
+      console.log('received data', data);
       this.program.output += data.replace('\\r', '');
     }.bind(this));
 
+    this.program.socket.on('data', function(data) {
+      console.log('s.d', data);
+      console.log('s.d.s', data.toString('utf-8'));
+    });
+
     this.program.on('exit', function() {
+      console.log('received exit');
       callback();
     });
   };
