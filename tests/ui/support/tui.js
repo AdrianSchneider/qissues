89400ebf9a4411ed971b9582e9cba8dcb9e6aeee
Adrian Schneider <siradrian@gmail.com>
2015-12-01 19:05:17 -0800

    non-interactive ui tests for now

--- tests/ui/support/tui.js
+++ tests/ui/support/tui.js
@@ -1,37 +1,33 @@
 'use strict';
 
 var pty = require('pty.js');
+var Promise = require('bluebird');
+var spawn   = require('child_process').spawn;
 
 function Tui() {
-  this.spawn = function(program, args, callback) {
+  this.spawnInteractive = function(program, args, callback) {
+    if(!args) args = [];
     if(this.program) this.program.destroy();
-    this.program = pty.spawn(program, args || [], {
+    this.program = pty.spawn('bash', [], {
       name: 'xterm-color',
       cols: 80,
       rows: 30,
       env: process.env
     });
 
-    console.log('program', this.program);
+    this.program.write(program + ' ' + args.map(function(a) { return '"' + a + '"'; }) + '\n');
 
     this.program.output = '';
     this.program.on('data', function(data) {
-      console.log('received data', data);
       this.program.output += data.replace('\\r', '');
     }.bind(this));
 
-    this.program.socket.on('data', function(data) {
-      console.log('s.d', data);
-      console.log('s.d.s', data.toString('utf-8'));
-    });
-
     this.program.on('exit', function() {
-      console.log('received exit');
       callback();
     });
   };
 }
 
 module.exports = function() {
-  this.World = Tui;
+  //this.World = Tui;
 };
