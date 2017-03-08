0f2d50986535feb7faeca65a168d59b3d252acbd
Adrian Schneider <siradrian@gmail.com>
2017-03-07 23:25:17 -0800

    async await WIP

--- src/domain/backend/jira/metadata.ts
+++ src/domain/backend/jira/metadata.ts
@@ -59,10 +59,8 @@ export default class JiraMetadata implements TrackerMetadata {
    * Get views from JIRA
    */
   public async getViews(options?): Promise<Object[]> {
-    return values(
-      (await this.client.get('/rest/greenhopper/1.0/rapidview'))
-        .data.views
-    );
+    let response = await this.client.get('/rest/greenhopper/1.0/rapidview');
+    return values(response.data.views);
   }
 
   /**
